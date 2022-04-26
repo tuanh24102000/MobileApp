39e20255d183b07f05ed5f321cf0b16d68442fad
tuanh <tuanh24102000@gmail.com>
2022-04-26 16:52:14 +0700

    Add detail and delete button

--- Components/ResultItem.js
+++ Components/ResultItem.js
@@ -2,16 +2,31 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 
 const ResultItem = ({result, navigation }) => {
+
+  const detailResult = () => {
+    navigation.navigate("Detail", {result})
+  }
   return (
     <View style= {styles.container}>
-      <View style={{ width: "90%" }}>
+      <View>
         <Text style={styles.Id}>Id: {result.Id}</Text>
         <Text style={styles.text}>Activity Name: {result.Activityname} </Text>
         <Text style={styles.text}>Location: {result.Location}</Text>
         <Text style={styles.text}>Date: {result.Date}</Text>
         <Text style={styles.text}>Time of attending: {result.Timeofattending}</Text>
         <Text style={styles.text}>Name of reporter: {result.Nameofreporter}</Text>
-      </View>
+        </View>
+
+    <TouchableOpacity onPress = {detailResult} style = {styles.button}>
+      <Text style = {{
+        alignItems:"center",
+        justifyContent:"center",
+        paddingHorizontal:20,
+        fontSize:12,
+        textTransform:"uppercase",
+        color:"white"
+      }}>Detail</Text>
+    </TouchableOpacity>
     </View>
   )
 }
@@ -41,5 +56,18 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         margin: 15,
         color:"green"
+      },
+      button: {
+        paddingLeft: 2,
+        width: 90,
+        height: 50,
+        borderWidth: 3,
+        backgroundColor: "blue",
+        fontSize: 250,
+        alignItems: "flex-end",
+        justifyContent: "center",
+        borderRadius: 5,
+        marginTop:20,
+        marginHorizontal:5,
       }
 })
--- Screens/Detail.js
+++ Screens/Detail.js
@@ -1,12 +1,64 @@
-import { View, Text } from 'react-native'
+import { View, Text, Alert, StyleSheet } from 'react-native'
 import React from 'react'
+import * as SQLite from "expo-sqlite"
+import CustomButton from '../Components/CustomButton'
 
-const Detail = () => {
+const db = SQLite.openDatabase("dbname", 1.0)
+
+const Detail = ({route, navigation}) => {
+  const {result} = route.params
+
+  const deleteItem = () => {
+    try {
+      db.transaction((tx) =>{
+        tx.executeSql("DELETE FROM DATABASE WHERE id =?",
+        [result.Id],
+        (tx, result) => {
+          Alert.alert("Deleted")
+          navigation.navigate("Home")
+        })
+      } ) 
+    }
+    catch (error) {
+      console.log(error)
+    }
+    }
+
   return (
-    <View>
-      <Text>Detail:</Text>
+             
+    <View style={styles.body}>
+    <View
+    style = {{
+    justifyContent:"center",
+    marginBottom: 150
+    }}>
+    <Text
+    style = {{
+    fontSize: 40,
+    fontWeight: "bold",
+    margin: 15,
+    }} >Detail</Text>
     </View>
-  )
+      <Text style = {styles.text}>Activity Name: {result.Activityname}</Text>
+      <Text style = {styles.text}>Location: {result.Location}</Text>
+      <Text style = {styles.text}>Date: {result.Date}</Text>
+      <Text style = {styles.text}>Time of attending: {result.Timeofattending}</Text>
+      <Text style = {styles.text}>Name of reporter: {result.Nameofreporter}</Text>
+      <CustomButton title="DELETE" handlePress = {deleteItem}/>
+    </View>
+  );
 }
+  const styles = StyleSheet.create({
+    body: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "flex-start",
+    },
+    text:{
+        fontSize:25,
+        justifyContent:"flex-start"
+    }
 
-export default Detail
+})
+
+export default Detail;
